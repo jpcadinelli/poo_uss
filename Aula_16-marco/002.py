04aed5251416e088d50ceae65716887b1c810daf
jpcadinelli <jpcadinelli@gmail.com>
2024-03-16 10:48:41 -0300

    deixando adicionado o enuncioado do ex2

---
+++ Aula_16-marco/002.py
@@ -0,0 +1,6 @@
+# Classe ContaBancaria:
+
+# Atributos: titular, numero_conta, saldo
+# Métodos: depositar, sacar
+# •	depositar(valor): adiciona o valor ao saldo da conta.
+# •	sacar(valor): subtrai o valor do saldo da conta, se houver saldo suficiente.
